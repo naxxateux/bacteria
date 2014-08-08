da3df44b66deeae23b28d518668fbfbefcd58239
naxxateux <damir.melnikov@gmail.com>
2014-08-08 10:56:30 +0400

    New screenshots

--- rb/get_bacteria_preview.rb
+++ rb/get_bacteria_preview.rb
@@ -4,7 +4,7 @@ width = b.execute_script('return screen.width;')
 height = b.execute_script('return screen.height;')
 b.driver.manage.window.move_to(0,0)
 b.driver.manage.window.resize_to(width,height)
-b.goto 'http://datalab/bacteria/bacteria.html?name=Acidaminococcus_intestini_RyC_MR95&view=ALL'
+b.goto 'http://datalab/bacteria/bacteria.html?name=Akkermansia_muciniphila_ATCC_BAA_835&view=ALL'
 sleep 5
 s = b.select_list :id => 'bacteria-selector'
 opts = s.options.map(&:text)
@@ -15,7 +15,7 @@ opts.each do |option|
 	b.screenshot.save 'ALL/' + option + '.png'
 end
 
-b.goto 'http://datalab/bacteria/bacteria.html?name=Acidaminococcus_intestini_RyC_MR95&view=CHN'
+b.goto 'http://datalab/bacteria/bacteria.html?name=Akkermansia_muciniphila_ATCC_BAA_835&view=CHN'
 sleep 5
 
 opts.each do |option|
@@ -24,7 +24,7 @@ opts.each do |option|
 	b.screenshot.save 'CHN/' + option + '.png'
 end
 
-b.goto 'http://datalab/bacteria/bacteria.html?name=Acidaminococcus_intestini_RyC_MR95&view=EUR'
+b.goto 'http://datalab/bacteria/bacteria.html?name=Akkermansia_muciniphila_ATCC_BAA_835&view=EUR'
 sleep 5
 
 opts.each do |option|
@@ -33,7 +33,7 @@ opts.each do |option|
 	b.screenshot.save 'EUR/' + option + '.png'
 end
 
-b.goto 'http://datalab/bacteria/bacteria.html?name=Acidaminococcus_intestini_RyC_MR95&view=RUS'
+b.goto 'http://datalab/bacteria/bacteria.html?name=Akkermansia_muciniphila_ATCC_BAA_835&view=RUS'
 sleep 5
 
 opts.each do |option|
@@ -42,7 +42,7 @@ opts.each do |option|
 	b.screenshot.save 'RUS/' + option + '.png'
 end
 
-b.goto 'http://datalab/bacteria/bacteria.html?name=Acidaminococcus_intestini_RyC_MR95&view=USA'
+b.goto 'http://datalab/bacteria/bacteria.html?name=Akkermansia_muciniphila_ATCC_BAA_835&view=USA'
 sleep 5
 
 opts.each do |option|
