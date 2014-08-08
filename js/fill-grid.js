07e3b41cc8d41f47eed93696b185b9aae9ce05ea
naxxateux <damir.melnikov@gmail.com>
2014-08-08 11:53:02 +0400

    Rubber width

--- js/fill-grid.js
+++ js/fill-grid.js
@@ -2,18 +2,20 @@ var bacteriaNames = [];
 var viewType = 'ALL';
 var imgWidth = 300,
     imgHeight = 300;
+var nOfPossibleRows = 5;
+var nOfPossibleColumns = Math.floor($(window).width() / (imgWidth + 30));
 
 var addContent = function(i, view) {
     var row = $('<div>').appendTo('#container').addClass('row');
 
-    for (var j = 0; j < 3; j++) {
+    for (var j = 0; j < nOfPossibleColumns; j++) {
         if ((i + j) < bacteriaNames.length) {
             var bacteriaName = bacteriaNames[i + j].split('/')[3].split('.')[0];
-            var column = $('<div>').appendTo(row).addClass('column-' + (j + 1));
-            $('<div>').appendTo(column).addClass('caption').html(bacteriaName);
+            var column = $('<div>').appendTo(row).addClass('column');
             var imageContainer = $('<div>').appendTo(column).addClass('image-container').fadeIn('slow');
             var link = $('<a href="bacteria.html?name=' + bacteriaName + '&view=' + view + '">').appendTo(imageContainer);
             $('<img src="img/' + view + '/' + bacteriaName + '.png" width="' + imgWidth + '" height="' + imgHeight + '">').appendTo(link);
+            $('<div>').appendTo(column).addClass('caption').html(bacteriaName);
         }
     }
 }
@@ -21,12 +23,12 @@ var addContent = function(i, view) {
 var drawSelectedViewTypeGrid = function() {
     $('#container').remove();
     $('body').append('<div id="container">');
-    var nOfPossibleRows = 5;
+    $('#container').width(nOfPossibleColumns * (imgWidth + 30));
     $(function() {
         // Read the file names
         $.get('php/get_images.php?view=' + viewType, function(data) {
             bacteriaNames = data;
-            for (var i = 0; i < nOfPossibleRows * 3; i += 3) {
+            for (var i = 0; i < nOfPossibleRows * nOfPossibleColumns; i += nOfPossibleColumns) {
                 if (i < bacteriaNames.length) {
                     addContent(i, viewType);
                 }
@@ -38,8 +40,8 @@ var drawSelectedViewTypeGrid = function() {
             var scrollTop = $(window).scrollTop();
             var scrollHeight = $(document).height();
             if (scrollTop === scrollHeight - $(window).height()) {
-                if ($('#container > *').length * 3 < bacteriaNames.length) {
-                    addContent($('#container > *').length * 3, viewType);
+                if ($('#container > *').length * nOfPossibleColumns < bacteriaNames.length) {
+                    addContent($('#container > *').length * nOfPossibleColumns, viewType);
                 }
             }
         });
